b5f19e9b7ff0624979d009431de0138a14476346
Adam Jaffe <36714896+CraftyJax@users.noreply.github.com>
2021-12-17 10:52:58 -0800

    Hack so RTB doesn't require GPS (#45)

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -1536,7 +1536,7 @@ public:
 
     bool init(bool ignore_checks) override;
     void run() override;
-    bool requires_GPS() const override { return true; }
+    bool requires_GPS() const override { return false; }
     bool has_manual_throttle() const override { return false; }
     bool allows_arming(bool from_gcs) const override { return false; }
     bool is_autopilot() const override { return true; }
